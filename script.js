790bda55758cc37d8f7079fa4f680113b1265c07
Cole Long <lolecong123@gmail.com>
2024-01-11 21:32:47 -0600

    update var to let

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 function paySalary() {
-  var employeeName = document.getElementById("name").value;
-  var employeeHours = document.getElementById("hours").value;
+  let employeeName = document.getElementById("name").value;
+  let employeeHours = document.getElementById("hours").value;
 
-  var basePayRate = 10.00;
-  var overtimeRate = 1.5;
+  let basePayRate = 10.0;
+  let overtimeRate = 1.5;
 
-  var regHours = Math.min(employeeHours, 40);
-  var overtimeHours = Math.max(employeeHours - regHours, 0);
+  let regHours = Math.min(employeeHours, 40);
+  let overtimeHours = Math.max(employeeHours - regHours, 0);
 
-  var regPay = basePayRate * regHours;
-  var overtimePay = basePayRate * overtimeRate * overtimeHours;
+  let regPay = basePayRate * regHours;
+  let overtimePay = basePayRate * overtimeRate * overtimeHours;
 
-  var salary = regPay + overtimePay;
+  let salary = regPay + overtimePay;
 
-  document.getElementById("name-output").textContent = "Employee Name: " + employeeName;
-  document.getElementById("pay").textContent = "Employee Gross Pay: $" + salary.toFixed(2);
+  document.getElementById("name-output").textContent =
+    "Employee Name: " + employeeName;
+  document.getElementById("pay").textContent =
+    "Employee Gross Pay: $" + salary.toFixed(2);
 }
 
 function areAllTimesheetsSubmitted(timesheets) {
-  return timesheets.length === 35 && timesheets.every((timesheet) => timesheet.submitted);
+  return (
+    timesheets.length === 35 &&
+    timesheets.every((timesheet) => timesheet.submitted)
+  );
 }
 
 function calculateFICATax(employees) {
   // Calculate the total gross pay for all the employees
-  var totalGrossPay = employees.reduce((total, emp) => total + emp.grossPay, 0);
+  let totalGrossPay = employees.reduce((total, emp) => total + emp.grossPay, 0);
 
   // FICA Tax Calculations
-  var ficaTax = totalGrossPay * 0.0765;
+  let ficaTax = totalGrossPay * 0.0765;
 
   return ficaTax.toFixed(2);
 }
